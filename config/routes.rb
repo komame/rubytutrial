fb3efb5ee87b9f4cd2e2cf5ba671aa17a5b1f819
こまめ <komame@animatelab.com>
2016-10-27 04:46:04 +0000

    homeとhelpのhtml編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  resources :microposts
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'users#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
